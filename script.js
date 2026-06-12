16b7dfde7b0f6e7fe62f2b42705349282590f952
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-12 17:44:50 +0530

    updated js file

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 function getWeather() {
     console.log("Weather function");
 }
+
+
+//IMPROVE JAVASCRIPT CODE
+
+function getWeather() {
+    const city = document.getElementById("city").value;
+
+    if (city === "") {
+        alert("Please enter a city");
+        return;
+    }
+
+    console.log(city);
+}
