7e2cb7d56ea9ae3215c0766c28cf6c976608d1c6
akalya-tech155 <akalyavlab182@gmail.com>
2026-06-12 17:51:57 +0530

    updated js file

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function getWeather() {
     console.log("Weather function");
 }
+
+
+
+async function getWeather() {
+
+    try {
+
+        const city = document.getElementById("city").value;
+
+        const response =
+            await fetch(`https://wttr.in/${city}?format=j1`);
+
+        const data = await response.json();
+
+        document.getElementById("weather").innerHTML =
+            `Temperature: ${data.current_condition[0].temp_C} °C`;
+
+    } catch (error) {
+
+        document.getElementById("weather").innerHTML =
+            "Failed to load weather data";
+
+    }
+}
